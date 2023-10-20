06d3683c39a2145855e1647c4a4199af3b43a5fe
rvinian <richa_vinian@intuit.com>
2023-10-20 14:50:20 +0530

    v1 interation

--- src/main/resources/db/migration/V1__init_db_schema.sql
+++ src/main/resources/db/migration/V1__init_db_schema.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS public.habit (
     id VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
     rrule VARCHAR(255) NOT NULL,
+    goal integer,
     created_at bigint NOT NULL,
     updated_at bigint,
     created_by VARCHAR(255) NOT NULL,
@@ -11,12 +13,12 @@ CREATE TABLE IF NOT EXISTS public.habit (
 CREATE TABLE IF NOT EXISTS public.task (
     id VARCHAR(255) NOT NULL,
     habit_id VARCHAR(255),
-    type VARCHAR(255) NOT NULL,
-    status VARCHAR(255) NOT NULL,
+    goal_progress integer,
+    is_completed boolean DEFAULT false,
     date bigint NOT NULL,
     repeat_start_date bigint NOT NULL,
     repeat_interval bigint NOT NULL,
-    CONSTRAINT task_scheduler_pk PRIMARY KEY(id)
+    CONSTRAINT task_pk PRIMARY KEY(id)
 );
 
 ALTER TABLE ONLY public.task
