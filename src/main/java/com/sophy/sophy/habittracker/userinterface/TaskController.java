83c551f8b0fd824404dfe04fa97dfe63c3bb1e88
rvinian <richa_vinian@intuit.com>
2024-02-08 21:32:27 +0530

    small setup

--- src/main/java/com/sophy/sophy/habittracker/userinterface/TaskController.java
+++ src/main/java/com/sophy/sophy/habittracker/userinterface/TaskController.java
@@ -2,16 +2,14 @@ package com.sophy.sophy.habittracker.userinterface;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(value = "v1/task", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(value = "v1/tasks", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class TaskController {
     @GetMapping()
-    public String getTasks() {
+    public String getHello() {
         return "Hi there, I am Sophy!";
     }
 }
--- src/main/resources/db/migration/V1__init_db_schema.sql
+++ src/main/resources/db/migration/V1__init_db_schema.sql
@@ -1,25 +1,23 @@
 CREATE TABLE IF NOT EXISTS public.habit (
     id VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    rrule VARCHAR(255) NOT NULL,
-    goal integer,
-    created_at bigint NOT NULL,
-    updated_at bigint,
-    created_by VARCHAR(255) NOT NULL,
+    frequency VARCHAR(255) NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    created_at date NOT NULL,
+    updated_at date,
+    created_by VARCHAR(255),
+    active boolean NOT NULL,
     CONSTRAINT habit_pk PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.task (
-    id VARCHAR(255) NOT NULL,
     habit_id VARCHAR(255),
-    goal_progress integer,
-    is_completed boolean DEFAULT false,
-    date bigint NOT NULL,
-    repeat_start_date bigint NOT NULL,
-    repeat_interval bigint NOT NULL,
-    CONSTRAINT task_pk PRIMARY KEY(id)
+    date date NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    active boolean NOT NULL,
+    CONSTRAINT task_pk PRIMARY KEY(habit_id, date)
 );
 
 ALTER TABLE ONLY public.task
-    ADD CONSTRAINT fk_task_on_habit FOREIGN KEY (habit_id) REFERENCES public.habit(id);
+    ADD CONSTRAINT fk_task_on_habit FOREIGN KEY (habit_id) REFERENCES public.habit(id) ON DELETE CASCADE;
